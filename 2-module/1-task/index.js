42f10ae8910ddc90ba22574754aae203b0a0309b
Aleksandr Solntsev <a.solntsev36@gmail.com>
2022-01-23 23:06:05 +0300

    Зфдача 1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,8 +1,8 @@
 function sumSalary(salaries) {
   let result = 0;
   for (let key in salaries) {
-    if (isFinite(salaries[key])) {
-      result += salaries[key];
+    if (Number.isFinite(salaries[key])) {
+      result = result + salaries[key];
     }
   }
   return result;
