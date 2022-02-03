818ed2a2eaebb40d5afa8c5ad9875a4ecb1389da
Aleksandr Solntsev <a.solntsev36@gmail.com>
2022-02-03 12:05:32 +0300

    JS + DOM elements

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
+/*
+Напишите функцию makeDiagonalRed, которая выделит красным цветом все ячейки в таблице по диагонали.
+Вам нужно получить из таблицы <table> все диагональные <td> и выделить их красным.
+Структуру элементов таблицы можно посмотреть в файле index.html, рядом с index.js.
+*/
+
+
 function makeDiagonalRed(table) {
-  // ваш код...
+
+  for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,17 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let tr = table.rows[i];
+
+    if (!tr.cells[3].hasAttribute('data-available')) {
+      tr.hidden = true;
+    }
+
+    (tr.cells[3].getAttribute('data-available') == 'true') ? tr.classList.add("available") : tr.classList.add("unavailable");
+
+    (tr.cells[2].innerHTML == 'm') ? tr.classList.add("male") : tr.classList.add("female");
+
+    if (+(tr.cells[1].innerHTML) < 18) {
+      tr.style.textDecoration = 'line-through';
+    }
+  }
 }
