0a3996727ca32bedf0564e075eca6b0283e577f8
Aleksandr Solntsev <a.solntsev36@gmail.com>
2022-02-07 19:50:12 +0300

    Hidden button

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,10 @@
+// Напишите функцию `hideSelf`, которая сделает так, чтобы кнопка с классом `hide-self-button`
+// скрывала себя по нажатию. Чтобы скрыть кнопку, добавьте ей атрибут `hidden`.
+
+
 function hideSelf() {
-  // ваш код...
+  const hiddenButton = document.querySelector('.hide-self-button');
+//  hiddenButton.onclick = function() {
+//  hiddenButton.hidden = true;
+  hiddenButton.onclick = () => hiddenButton.hidden = true;
 }
