0d70a3bca8f9fa6276a2738a8934d65daeb61f45
Aleksandr Solntsev <a.solntsev36@gmail.com>
2022-04-18 16:32:31 +0300

    Carousel

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,6 @@
+// Напишите функцию `toggleText`, которая сделает так, чтобы при первом нажатии на кнопку с классом `toggle-text-button` текст
+//`<div id="text">Текст</div>` исчезал, а при повторном нажатии появлялся. Чтобы скрыть текст, добавьте ему атрибут `hidden` и наоборот.
+
 function toggleText() {
   const text = document.querySelector('#text');
   const button = document.querySelector('.toggle-text-button');
